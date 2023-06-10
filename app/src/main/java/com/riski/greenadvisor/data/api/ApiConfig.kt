48edceec9171adeea3af3cac3a9e86c931ac8746
Riski Apriyanto <lastqwe779@gmail.com>
2023-06-10 12:38:00 +0700

    Create Function in Api

--- app/src/main/java/com/riski/greenadvisor/data/api/ApiConfig.kt
+++ app/src/main/java/com/riski/greenadvisor/data/api/ApiConfig.kt
@@ -1,4 +1,37 @@
 package com.riski.greenadvisor.data.api
 
+import com.riski.greenadvisor.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class ApiConfig {
+    companion object{
+        fun getApiService(): ApiService {
+            val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BuildConfig.Api_MOBILE)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+
+    fun getApiService(): ApiService {
+        val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.Api_MOBILE)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
 }
--- app/src/main/java/com/riski/greenadvisor/data/api/ApiService.kt
+++ app/src/main/java/com/riski/greenadvisor/data/api/ApiService.kt
@@ -1,4 +1,24 @@
 package com.riski.greenadvisor.data.api
 
-class ApiService {
+import com.riski.greenadvisor.data.response.LoginResponse
+import com.riski.greenadvisor.data.response.RegisterResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @FormUrlEncoded
+    @POST("register")
+    fun register(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String,
+        @Field("c_password") c_password: String
+    ): Call<RegisterResponse>
+
+    @FormUrlEncoded
+    @POST("login")
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<LoginResponse>
 }
