3b8268fcc91e1aec167f0ab3064eb413032c6f68
Riski Apriyanto <lastqwe779@gmail.com>
2023-06-10 12:51:51 +0700

    Fix Some function in HomeFragment

--- app/src/main/java/com/riski/greenadvisor/ui/home/HomeFragment.kt
+++ app/src/main/java/com/riski/greenadvisor/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.riski.greenadvisor.ui.home
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,6 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,17 +19,22 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.riski.greenadvisor.BetaActivity
 import com.riski.greenadvisor.R
+import com.riski.greenadvisor.data.Preference
 import com.riski.greenadvisor.data.UserData
 import com.riski.greenadvisor.data.greetings.ArticlesData
+import com.riski.greenadvisor.data.response.DataLogin
 import com.riski.greenadvisor.databinding.FragmentHomeBinding
 import com.riski.greenadvisor.ui.detail.showlist.DetailShowListArticlesActivity
 import com.riski.greenadvisor.ui.detail.showlist.DetailShowListPlantActivity
 import com.riski.greenadvisor.ui.home.adapter.ArticlesAdapter
 import com.riski.greenadvisor.ui.home.adapter.HomeViewModelAdapter
 import com.riski.greenadvisor.ui.home.adapter.PlantCareAdapter
+import com.riski.greenadvisor.ui.login.LoginActivity
 
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore("settings")
 class HomeFragment : Fragment() {
-
+    private lateinit var dataStore: DataStore<Preferences>
+    private lateinit var user: DataLogin
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
@@ -33,13 +42,13 @@ class HomeFragment : Fragment() {
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
         (activity as AppCompatActivity).supportActionBar?.hide()
         @Suppress("DEPRECATION")
         setHasOptionsMenu(true)
 //        (activity as AppCompatActivity).invalidateOptionsMenu()
+        homeUserViewModel()
         setViewPager()
         changeName()
         setArticles()
@@ -47,9 +56,38 @@ class HomeFragment : Fragment() {
         clickArticles()
         clickPlantCare()
         notification()
+        userLogin()
+
 
         return root
 
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        dataStore = requireContext().dataStore
+    }
+
+    private fun homeUserViewModel() {
+        homeViewModel = ViewModelProvider(this, HomeViewModelFactory(Preference.getInstance(dataStore), requireContext()))[HomeViewModel::class.java]
+    }
+
+    private fun userLogin() {
+        val pref = Preference.getInstance(dataStore)
+        homeViewModel = ViewModelProvider(this, HomeViewModelFactory(pref, requireContext()))[homeViewModel::class.java]
+        homeViewModel.getUser().observe(requireActivity()) { user->
+            this.user = user
+            if (!user.isLogin) {
+                startActivity(Intent(requireContext(), LoginActivity::class.java))
+                requireActivity().finish()
+            } else {
+                this.user = DataLogin(
+                    user.name,
+                    user.token,
+                    true
+                )
+            }
+        }
     }
 
     private fun setViewPager() {
---
+++ app/src/main/java/com/riski/greenadvisor/ui/home/HomeViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.riski.greenadvisor.ui.home
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.riski.greenadvisor.data.Preference
+
+class HomeViewModelFactory(private val preference: Preference, private val context: Context): ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
+            return HomeViewModel(preference) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
+}
