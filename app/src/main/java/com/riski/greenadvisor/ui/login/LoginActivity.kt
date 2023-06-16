fa1ab4075706cb5d745a7396a541e41a3f92f765
Riski Apriyanto <lastqwe779@gmail.com>
2023-06-16 09:58:07 +0700

    Update LoginActivity

--- app/src/main/java/com/riski/greenadvisor/ui/login/LoginActivity.kt
+++ app/src/main/java/com/riski/greenadvisor/ui/login/LoginActivity.kt
@@ -1,10 +1,13 @@
 package com.riski.greenadvisor.ui.login
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
@@ -27,9 +30,11 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var loginViewModel: LoginViewModel
 
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setContentView(binding.root)
         supportActionBar?.hide()
         sharedPreferences = getSharedPreferences("GreetingsPrefs", Context.MODE_PRIVATE)
@@ -40,6 +45,7 @@ class LoginActivity : AppCompatActivity() {
         btnEnabled()
         editText()
         progressBar()
+        changeLanguage()
 
     }
 
@@ -53,6 +59,12 @@ class LoginActivity : AppCompatActivity() {
 
     private fun loginUserViewModel() {
         loginViewModel = ViewModelProvider(this, LoginViewModelFactory(Preference.getInstance(dataStore), this))[LoginViewModel::class.java]
+    }
+
+    private fun changeLanguage() {
+        binding.loginLanguageFab.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+        }
     }
 
     private fun editText() {
@@ -89,17 +101,19 @@ class LoginActivity : AppCompatActivity() {
         binding.loginButton.setOnClickListener {
             val email = binding.loginEmail.text.toString()
             val password = binding.loginPassword.text.toString()
+            blur()
 
             loginViewModel.login(email, password)
             loginViewModel.response.observe(this@LoginActivity) { login->
                 if (login != null) {
                     if (login.success) {
-                       progressBar()
-                       Toast.makeText(this@LoginActivity, getString(R.string.login_success), Toast.LENGTH_SHORT).show()
+                        progressBar()
                         startActivity(Intent(this, MainActivity::class.java))
                         finish()
+                        Toast.makeText(this@LoginActivity, getString(R.string.login_success), Toast.LENGTH_SHORT).show()
                     } else {
                         progressBar()
+                        noBlur()
                         Toast.makeText(this@LoginActivity, getString(R.string.login_failed), Toast.LENGTH_SHORT).show()
                     }
                 }
@@ -118,13 +132,40 @@ class LoginActivity : AppCompatActivity() {
         loginViewModel.loading.observe(this) {
             binding.apply {
                 if (it) {
-                    loginLoading.visibility = View.VISIBLE
-                    loginButton.visibility = View.GONE
+                loginLoading.visibility = View.VISIBLE
+                loginLoading.requestFocus()
+                loginButton.visibility = View.GONE
                 } else {
                     loginLoading.visibility = View.GONE
                     loginButton.visibility = View.VISIBLE
                 }
             }
         }
+    }
+
+    private fun blur() {
+        binding.loginImage.alpha = 0.3f
+        binding.loginWelcome.alpha = 0.3f
+        binding.loginWelcome1.alpha = 0.3f
+        binding.loginEmail.alpha = 0.3f
+        binding.loginEmailText.alpha = 0.3f
+        binding.loginEmail.alpha = 0.3f
+        binding.loginPassword.alpha = 0.3f
+        binding.loginPasswordText.alpha = 0.3f
+        binding.loginHave.alpha = 0.3f
+        binding.loginButton.alpha = 0.3f
+    }
+
+    private fun noBlur() {
+        binding.loginImage.alpha = 1f
+        binding.loginWelcome.alpha = 1f
+        binding.loginWelcome1.alpha = 1f
+        binding.loginEmail.alpha = 1f
+        binding.loginEmailText.alpha = 1f
+        binding.loginEmail.alpha = 1f
+        binding.loginPassword.alpha = 1f
+        binding.loginPasswordText.alpha = 1f
+        binding.loginHave.alpha = 1f
+        binding.loginButton.alpha = 1f
     }
 }
