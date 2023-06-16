88d30ad8e9d2ae20811bd5f947420aed73b34762
Riski Apriyanto <lastqwe779@gmail.com>
2023-06-16 10:11:14 +0700

    fixed some bugs

--- app/src/main/java/com/riski/greenadvisor/ui/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/riski/greenadvisor/ui/splash/SplashScreenActivity.kt
@@ -2,19 +2,17 @@
 
 package com.riski.greenadvisor.ui.splash
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.animation.AccelerateDecelerateInterpolator
+import android.os.Handler
+import android.os.Looper
+import android.preference.PreferenceManager
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModelProvider
 import com.riski.greenadvisor.MainActivity
@@ -24,131 +22,54 @@ import com.riski.greenadvisor.ui.greetings.GreetingsActivity
 import com.riski.greenadvisor.ui.home.HomeViewModel
 import com.riski.greenadvisor.ui.home.HomeViewModelFactory
 import com.riski.greenadvisor.ui.login.LoginActivity
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.runBlocking
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("settings")
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
 
-    private val splashDuration = 2000L
+    private val splashDuration = 1000L
     private lateinit var binding: ActivitySplashScreenBinding
     private lateinit var splashScreenViewModel: HomeViewModel
 
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         supportActionBar?.hide()
 
-        animationSet()
-
+        Handler(Looper.getMainLooper()).postDelayed({
+            checkGreetingsActivity()
+        }, splashDuration)
     }
 
-    override fun onResume() {
-        super.onResume()
-        checkFirstRun()
-    }
+    private fun checkGreetingsActivity() {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val greetingsShown = sharedPreferences.getBoolean("GREETINGS_SHOWN", false)
 
-    private fun checkFirstRun() {
-        val isFirstRun = readFirstRunStatus()
-        if (isFirstRun) {
-            saveFirstRunStatus(false)
+        if (greetingsShown) {
+            saveLogin()
+        } else {
+            val editor = sharedPreferences.edit()
+            editor.putBoolean("GREETINGS_SHOWN", true)
+            editor.apply()
             startActivity(Intent(this, GreetingsActivity::class.java))
             finish()
-        } else {
-            countinueToNextActivity()
         }
     }
 
-    private fun readFirstRunStatus(): Boolean {
-        val dataStoreKey = (booleanPreferencesKey ("FIRST_RUN_STATUS"))
-        val dataStore = dataStore
-        val isFirstRun = runBlocking {
-            dataStore.data.map { preference ->
-                preference[dataStoreKey] ?: true
-            }.first()
-        }
-        return isFirstRun
-    }
-
-    private fun saveFirstRunStatus(isFirstRun: Boolean) {
-        val dataStoreKey = booleanPreferencesKey("FIRST_RUN_STATUS")
-        val dataStore = dataStore
-        runBlocking {
-            dataStore.edit { preferences ->
-                preferences[dataStoreKey] = isFirstRun
-            }
-        }
-    }
-
-    private fun countinueToNextActivity() {
+    private fun saveLogin() {
         splashScreenViewModel = ViewModelProvider(this, HomeViewModelFactory(Preference.getInstance(dataStore), this))[HomeViewModel::class.java]
-        splashScreenViewModel.getUser().observe(this) { user ->
-            val targetClass = if (user.isLogin) {
-                MainActivity::class.java
+
+        splashScreenViewModel.getUser().observe(this) {
+            if (it.isLogin) {
+                startActivity(Intent(this, MainActivity::class.java))
             } else {
-                LoginActivity::class.java
+                startActivity(Intent(this, LoginActivity::class.java))
             }
-            val intent = Intent(this, targetClass)
-            startActivity(intent)
             finish()
         }
-    }
-
-//    private fun saveLogin() {
-//        splashScreenViewModel = ViewModelProvider(this, HomeViewModelFactory(Preference.getInstance(dataStore), this))[HomeViewModel::class.java]
-//
-//        splashScreenViewModel.getUser().observe(this) {
-//            if (it.isLogin) {
-//                startActivity(Intent(this, MainActivity::class.java))
-//                finish()
-//            } else {
-//                startActivity(Intent(this, LoginActivity::class.java))
-//                finish()
-//            }
-//        }
-//    }
-
-    private fun animationSet() {
-        val splashImage = binding.splashImage
-
-        val animSlideLeft = ObjectAnimator.ofFloat(splashImage, "translationX", 0f, -100f)
-        animSlideLeft.duration = splashDuration / 2
-        animSlideLeft.interpolator = AccelerateDecelerateInterpolator()
-
-        val animSlideRight = ObjectAnimator.ofFloat(splashImage, "translationX", -100f, 100f)
-        animSlideRight.duration = splashDuration / 2
-        animSlideRight.interpolator = AccelerateDecelerateInterpolator()
-
-        animSlideLeft.start()
-
-        animSlideLeft.addUpdateListener {
-            if (it.animatedFraction >= 1f) {
-                animSlideRight.start()
-            }
-        }
-
-        animSlideRight.addListener(object : AnimatorListenerAdapter() {
-            override fun onAnimationEnd(animation: Animator) {
-                countinueToNextActivity()
-            }
-//            override fun onAnimationEnd(animation: Animator) {
-//                val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this@SplashScreenActivity)
-//                val joinStatus = sharedPreferences.getBoolean("JOIN_STATUS", false)
-//
-//
-//                val targetClass = if (joinStatus) {
-//                    RegisterActivity::class.java
-//                } else {
-//                    GreetingsActivity::class.java
-//                }
-//                val intent = Intent(this@SplashScreenActivity, targetClass)
-//                startActivity(intent)
-//                finish()
-//            }
-        })
     }
 }
